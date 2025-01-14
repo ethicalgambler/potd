14aa48280e3d1822670223c33965ac426c565dbc
Michael Kaufman <mkaufman@labx.com>
2025-01-14 10:16:18 -0500

    Deploy test commits

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.set_page_config(layout="wide")
 
 
 # url = "https://docs.google.com/spreadsheets/d/1XoVHcy6qqwKKT7HiIb5CKwv32_1Ce1fhl5XoPW-lREI/edit?usp=sharing" 
-url = "https://docs.google.com/spreadsheets/d/1PfOBLuiQcNfOdTgdTAkgw8vqivz7liHQimlzr4AgBHc/edit?usp=sharing"
+url = "https://docs.google.com/spreadsheets/d/1KD-sPzMceSj-rWafb-6FPzhoaAQ48i2r2p9oJDQEoUY/edit?usp=sharing"
 conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read(spreadsheet=url, usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8])
 # df['Date'] = pd.to_datetime(df['Date'])
