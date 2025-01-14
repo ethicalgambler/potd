c7e7fef8f0e3354e2c7e3068a3a9be2557b3fe73
Michael Kaufman <mkaufman@labx.com>
2025-01-14 10:10:30 -0500

    Deploy test commits

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ import calendar
 import numpy as np
 from streamlit_gsheets import GSheetsConnection
 
-st.set_page_config(layout="centered")
 st.set_page_config(layout="wide")
 
 
 
-url = "https://docs.google.com/spreadsheets/d/1XoVHcy6qqwKKT7HiIb5CKwv32_1Ce1fhl5XoPW-lREI/edit?usp=sharing" 
-# url = "https://docs.google.com/spreadsheets/d/1KD-sPzMceSj-rWafb-6FPzhoaAQ48i2r2p9oJDQEoUY/edit?usp=sharing"
+
+# url = "https://docs.google.com/spreadsheets/d/1XoVHcy6qqwKKT7HiIb5CKwv32_1Ce1fhl5XoPW-lREI/edit?usp=sharing" 
+url = "https://docs.google.com/spreadsheets/d/1KD-sPzMceSj-rWafb-6FPzhoaAQ48i2r2p9oJDQEoUY/edit?usp=sharing"
 conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read(spreadsheet=url, usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8])
 df['Date'] = pd.to_datetime(df['Date'])
