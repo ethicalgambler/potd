53f2a6edda3a75019d4ba7fe215fd5ffcf16c32c
Michael Kaufman <mkaufman@labx.com>
2025-01-14 09:55:53 -0500

    Deploy test commits

--- app.py
+++ app.py
@@ -16,18 +16,8 @@ st.set_page_config(layout="wide")
 url = "https://docs.google.com/spreadsheets/d/1XoVHcy6qqwKKT7HiIb5CKwv32_1Ce1fhl5XoPW-lREI/edit?usp=sharing" 
 # url = "https://docs.google.com/spreadsheets/d/1KD-sPzMceSj-rWafb-6FPzhoaAQ48i2r2p9oJDQEoUY/edit?usp=sharing"
 conn = st.connection("gsheets", type=GSheetsConnection)
-df = conn.read(spreadsheet=url, usecols=[0, 1, 2, 3, 4])
+df = conn.read(spreadsheet=url, usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8])
 df['Date'] = pd.to_datetime(df['Date'])
-
-data_filter = st.radio("Filter Data by POTD", ("All Data", "POTD Only"))
-
-if data_filter == "POTD Only":
-    df_filtered = df[df['POTD'] == 1]
-else:
-    df_filtered = df
-
-# Filter for 2025 data for visuals
-df_2025 = df[df['Date'].dt.year == 2025]
 
 
 #df_filtered['Date'] = df_filtered['Date'].dt.strftime('%m/%d/%Y')
