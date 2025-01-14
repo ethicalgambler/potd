1f047758f01a5a5ac1ef0ac6a67b44075689a3aa
Michael Kaufman <mkaufman@labx.com>
2025-01-14 09:55:22 -0500

    Deploy test commits

--- app.py
+++ app.py
@@ -16,11 +16,9 @@ st.set_page_config(layout="wide")
 url = "https://docs.google.com/spreadsheets/d/1XoVHcy6qqwKKT7HiIb5CKwv32_1Ce1fhl5XoPW-lREI/edit?usp=sharing" 
 # url = "https://docs.google.com/spreadsheets/d/1KD-sPzMceSj-rWafb-6FPzhoaAQ48i2r2p9oJDQEoUY/edit?usp=sharing"
 conn = st.connection("gsheets", type=GSheetsConnection)
-df = conn.read(spreadsheet=url, usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+df = conn.read(spreadsheet=url, usecols=[0, 1, 2, 3, 4])
 df['Date'] = pd.to_datetime(df['Date'])
 
-st.header("Picks Made")
-# Radio button to filter full data by POTD
 data_filter = st.radio("Filter Data by POTD", ("All Data", "POTD Only"))
 
 if data_filter == "POTD Only":
