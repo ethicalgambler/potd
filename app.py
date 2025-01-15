5e2202369528c676902da3e1ea92038b1a7dbc8a
Michael Kaufman <mkaufman@labx.com>
2025-01-15 15:51:44 -0500

    Deploy test commits 2025-01-15 15:51:44

--- app.py
+++ app.py
@@ -5,24 +5,21 @@ import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime
 import calendar
+import pathlib
 import numpy as np
 from streamlit_gsheets import GSheetsConnection
 
 
 
-st.markdown("*Streamlit* is **really** ***cool***.")
-st.markdown('''
-    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
-    :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
-st.markdown("Here's a bouquet &mdash;\
-            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+# Function to load CSS from the 'assets' folder
+def load_css(file_path):
+    with open(file_path) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-multi = '''If you end a line with two spaces,
-a soft return is used for the next line.
 
-Two (or more) newline characters in a row will result in a hard return.
-'''
-st.markdown(multi)
+# Load the external CSS
+css_path = pathlib.Path("assets/styles.css")
+load_css(css_path)
 
 
 
