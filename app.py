6545b6ec58b7da930ebe629f3120dad5bea57b53
Michael Kaufman <mkaufman@labx.com>
2025-01-15 16:21:15 -0500

    Deploy test commits 2025-01-15 16:21:15

--- app.py
+++ app.py
@@ -13,9 +13,12 @@ from streamlit_gsheets import GSheetsConnection
 
 
 
-# Load page config
+# Load page config start
 st.set_page_config(layout="wide")
 # st.set_page_config(layout="centered", initial_sidebar_state="expanded", theme={"base": "light"})
+# Load page config end
+
+
 st.error("This is an error alert!")
 st.warning("This is a warning alert!")
 st.success("This is a success alert!")
@@ -25,13 +28,13 @@ theme = st_theme()
 st.write(theme)
 
 
-
+# Custom CSS fucntion start
 def load_css(file_path):
     with open(file_path) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 css_path = pathlib.Path("assets/styles.css")
 load_css(css_path)
-
+# Custom CSS fucntion end
 
 
 
