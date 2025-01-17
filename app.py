6552f9c77fac9d023bd28813af8fa103218f2de5
Michael Kaufman <mkaufman@labx.com>
2025-01-17 18:02:32 -0500

    Deploy test commits 2025-01-17 18:02:32

--- app.py
+++ app.py
@@ -17,13 +17,7 @@ from streamlit_gsheets import GSheetsConnection
 st.set_page_config(layout="wide")
 # Load page config end
 
-# Custom CSS fucntion start
-def load_css(file_path):
-    with open(file_path) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-css_path = pathlib.Path("assets/styles.css")
-load_css(css_path)
-# Custom CSS fucntion end
+
 
 # Script to identify if light mode is being used
 theme = st_theme()
@@ -68,3 +62,14 @@ else:
     # st.dataframe(df_today)
 
 
+
+
+
+
+# Custom CSS fucntion start
+def load_css(file_path):
+    with open(file_path) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+css_path = pathlib.Path("assets/styles.css")
+load_css(css_path)
+# Custom CSS fucntion end
