957f0be4c0255c0c8d1cba4c2cbf9b14a26cea48
Michael Kaufman <mkaufman@labx.com>
2025-01-16 21:33:11 -0500

    Deploy test commits 2025-01-16 21:33:11

--- app.py
+++ app.py
@@ -17,6 +17,15 @@ st.set_page_config(layout="wide")
 # st.set_page_config(layout="centered", initial_sidebar_state="expanded", theme={"base": "light"})
 # Load page config end
 
+# Custom CSS fucntion start
+def load_css(file_path):
+    with open(file_path) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+css_path = pathlib.Path("assets/styles.css")
+load_css(css_path)
+# Custom CSS fucntion end
+
+
 
 # st.error("This is an error alert!")
 # st.warning("This is a warning alert!")
@@ -34,54 +43,40 @@ if background_color != "#ffffff":
     st.warning("This is a warning alert!")
 else:
 
+    
 
 
+    # st.title("Light Mode Only App")
 
+    # leguppicks
+    # url = "https://docs.google.com/spreadsheets/d/1XoVHcy6qqwKKT7HiIb5CKwv32_1Ce1fhl5XoPW-lREI/edit?usp=sharing" 
 
-# Custom CSS fucntion start
-def load_css(file_path):
-    with open(file_path) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-css_path = pathlib.Path("assets/styles.css")
-load_css(css_path)
-# Custom CSS fucntion end
+    # POTD original
+    # url = "https://docs.google.com/spreadsheets/d/1KD-sPzMceSj-rWafb-6FPzhoaAQ48i2r2p9oJDQEoUY/edit?usp=sharing"
 
+    # POTD test
+    url = "https://docs.google.com/spreadsheets/d/1PfOBLuiQcNfOdTgdTAkgw8vqivz7liHQimlzr4AgBHc/edit?usp=sharing"
 
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    df = conn.read(spreadsheet=url, usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
 
+    column_names = [
+        'Capper', 'Tot W/L', 'L10', 'STRK', 'Avg U', 'ROI %', 'Date', 'League / Sport', 'Pick /Prop', 'Units', 'US', 'Dec', 'W', 'L', 'P', 'W/L', 'Notes'
+    ]
 
+    df.columns = column_names
 
+    # Convert the 'Date' column to datetime format (assuming the date format is MM/DD/YY)
+    df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%y')
 
-st.title("Light Mode Only App")
+    # Get today's date
+    today = pd.to_datetime(datetime.today().strftime('%m/%d/%y'), format='%m/%d/%y')
 
-# leguppicks
-# url = "https://docs.google.com/spreadsheets/d/1XoVHcy6qqwKKT7HiIb5CKwv32_1Ce1fhl5XoPW-lREI/edit?usp=sharing" 
+    # Filter the DataFrame to show only rows from today
+    df_today = df[df['Date'] == today]
 
-# POTD original
-# url = "https://docs.google.com/spreadsheets/d/1KD-sPzMceSj-rWafb-6FPzhoaAQ48i2r2p9oJDQEoUY/edit?usp=sharing"
-
-# POTD test
-url = "https://docs.google.com/spreadsheets/d/1PfOBLuiQcNfOdTgdTAkgw8vqivz7liHQimlzr4AgBHc/edit?usp=sharing"
-
-conn = st.connection("gsheets", type=GSheetsConnection)
-df = conn.read(spreadsheet=url, usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
-
-column_names = [
-    'Capper', 'Tot W/L', 'L10', 'STRK', 'Avg U', 'ROI %', 'Date', 'League / Sport', 'Pick /Prop', 'Units', 'US', 'Dec', 'W', 'L', 'P', 'W/L', 'Notes'
-]
-
-df.columns = column_names
-
-# Convert the 'Date' column to datetime format (assuming the date format is MM/DD/YY)
-df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%y')
-
-# Get today's date
-today = pd.to_datetime(datetime.today().strftime('%m/%d/%y'), format='%m/%d/%y')
-
-# Filter the DataFrame to show only rows from today
-df_today = df[df['Date'] == today]
-
-# Display the filtered DataFrame
-st.dataframe(df_today, hide_index=True)
-# st.dataframe(df_today)
+    # Display the filtered DataFrame
+    st.dataframe(df_today, hide_index=True)
+    # st.dataframe(df_today)
 
 
